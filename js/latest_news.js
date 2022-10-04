e40c8f26248e40fdd773d89dd46528d5d17064cc
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-10-04 18:02:36 +0200

    Configuring the API method and headers

---
+++ js/latest_news.js
@@ -0,0 +1,9 @@
+(function latestNews() {
+  const settings = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "223c608d4dmsh848549f5afe454fp1ec308jsn857286b35c64",
+      "X-RapidAPI-Host": "newscatcher.p.rapidapi.com",
+    },
+  };
+})();
