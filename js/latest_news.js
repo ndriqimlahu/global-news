91fe48ef31201a5eef5ee69f9a0dfd529866869b
Ndriçim Lahu <ndriqim_lahu@hotmail.com>
2022-10-04 18:05:14 +0200

    Fetching data from API for latest news articles

--- js/latest_news.js
+++ js/latest_news.js
@@ -33,6 +33,39 @@
         .querySelector(".banner-article")
         .insertAdjacentHTML("afterbegin", response_data);
     })
+
+    // Latest News
+    fetch('https://newscatcher.p.rapidapi.com/v1/search?q=flash&lang=en&sort_by=date&page=1&page_size=3&media=True', settings)
+    .then((response) => response.json())
+    .then((response) => {
+      const response_data = response.articles
+        .map((articles) => {
+          return `
+          <div class="col-sm-12">
+            <div class="border-bottom pb-4 pt-4">
+              <div class="row">
+                <div class="col-sm-4">
+                  <div class="rotate-img">
+                    <img
+                      src="${articles.media}"
+                      alt="Latest News Image"
+                      class="img-fluid latest-article-media" />
+                  </div>
+                </div>
+                <div class="col-sm-8">
+                  <a href="${articles.link}" target="_blank">
+                    <h5 class="font-weight-600 mb-1 latest-article-title">${articles.title}</h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>`;
+        })
+        .join("");
+      document
+        .querySelector(".latest-articles")
+        .insertAdjacentHTML("afterbegin", response_data);
+    })
     
     .catch((error) =>
       console.error("Something went wrong, please try again later.", error)
